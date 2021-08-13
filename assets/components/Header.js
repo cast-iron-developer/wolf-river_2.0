7056a7ce11dfd349af5dc7f858666f3c8a3c6095
Sam ODonnell <sodonnell5214@gmail.com>
2021-08-13 06:57:55 -0400

    Redesigned Header to be a better funcitonal component

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -1,65 +1,100 @@
-import { header } from '../js/header';
-import Image from 'next/image';
-import Link from 'next/link';
+import { header } from "../js/header";
+import Image from "next/image";
+import Link from "next/link";
 import logo from "../images/logo.png";
-import React from "react"
+import React, { useEffect, useState } from "react";
 
-class Header extends React.PureComponent {
-  componentDidMount() {
-    header()
-  }
+const Header = ({originPage}) => {
+    // https://stackoverflow.com/questions/29725828/update-style-of-a-component-onscroll-in-react-js
+    const [offset, setOffset] = useState(0);
+    let body, menu_btn, header;
 
-  render() {
+    console.log(originPage);
+
+    useEffect(() => {
+        body = document.querySelector("body");
+        menu_btn = document.querySelector("#menu-btn");
+        header = document.querySelector('.main-header');
+
+        window.onscroll = () => {
+            setOffset(window.pageYOffset);
+        };
+
+        checkScrollOffset(offset);
+    });
+
+    const checkScrollOffset = (num) => {
+        num > 10 ? header.classList.add('scrolled') : header.classList.remove('scrolled');
+    };
+
+    const toggleHeader = (e) => {
+        !body.classList.contains("no-scroll")
+            ? body.classList.add("no-scroll")
+            : body.classList.remove("no-scroll");
+    };
+
+    const toggleMenuFromNav = (e) => {
+        if (menu_btn.checked) {
+            menu_btn.checked = false;
+        }
+
+        if (body.classList.contains("no-scroll")) {
+            body.classList.remove("no-scroll");
+        }
+    };
+
     return (
-      <header id={`${this.props.originPage}-page`} className="main-header">
-        <div className="logo">
-          <Link href="/">
-            <a className="site-logo-link">
-              <Image className="site-logo" width="200" height="70" layout="fixed" src={logo} alt="Wolf River Psychology" />
-            </a>
-          </Link>
-        </div>
+        <header id={`${originPage}-page`} className="main-header">
+            <div className="logo">
+                <Link href="/">
+                    <a className="site-logo-link">
+                        <Image
+                            className="site-logo"
+                            width="200"
+                            height="70"
+                            layout="fixed"
+                            src={logo}
+                            alt="Wolf River Psychology"
+                        />
+                    </a>
+                </Link>
+            </div>
 
-        <input type="checkbox" id="menu-btn" className="menu-btn" />
-        <label htmlFor="menu-btn" className="menu-icon">
-          <span className="menu-icon__line"></span>
-        </label>
-        <nav className="nav-links">
-          <ul>
-            <li className="nav-link">
-              <Link href="/professional-profile">
-                <a>
-                  Profile
-               </a>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link href="/about-psychotherapy">
-                <a>
-                  About
-              </a>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link href="/services">
-                <a>
-                  Services
-              </a>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link href="/contact">
-                <a>
-                  Contact
-              </a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+            <input
+                type="checkbox"
+                id="menu-btn"
+                className="menu-btn"
+                onClick={toggleHeader}
+            />
+            <label htmlFor="menu-btn" className="menu-icon">
+                <span className="menu-icon__line"></span>
+            </label>
+            <nav className="nav-links">
+                <ul>
+                    <li className="nav-link">
+                        <Link href="/professional-profile">
+                            <a onClick={toggleMenuFromNav}>Profile</a>
+                        </Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link href="/about-psychotherapy">
+                            <a onClick={toggleMenuFromNav}>About</a>
+                        </Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link href="/services">
+                            <a onClick={toggleMenuFromNav}>Services</a>
+                        </Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link href="/contact">
+                            <a onClick={toggleMenuFromNav}>Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
-      </header>
-    )
-  }
-}
-
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,81 +1,114 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 import Layout from "../assets/components/layout";
-import SingleColumn from '../assets/components/singleColumn';
-import OffsetColumns from '../assets/components/OffsetColumns';
-import FullWidthColumn from '../assets/components/FullWidthColumn';
-import LeftOffset from '../assets/components/partials/LeftOffset'
-import RightOffset from '../assets/components/partials/RightOffset'
-import Blockquote from '../assets/components/partials/Blockquote';
-import SpaSharpIcon from '@material-ui/icons/SpaSharp';
-import NightsStayIcon from '@material-ui/icons/NightsStay';
-import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import SingleColumn from "../assets/components/singleColumn";
+import OffsetColumns from "../assets/components/OffsetColumns";
+import FullWidthColumn from "../assets/components/FullWidthColumn";
+import LeftOffset from "../assets/components/partials/LeftOffset";
+import RightOffset from "../assets/components/partials/RightOffset";
+import Blockquote from "../assets/components/partials/Blockquote";
+import SpaSharpIcon from "@material-ui/icons/SpaSharp";
+import NightsStayIcon from "@material-ui/icons/NightsStay";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 
-import classes from '../assets/scss/modules/index.module.scss';
-import drColdwell from '../assets/images/dr-coldwell-homepage.jpg';
+import classes from "../assets/scss/modules/index.module.scss";
+import drColdwell from "../assets/images/dr-coldwell-homepage.jpg";
 
 const Homepage = () => (
     <>
-      <Head>
-          <title>Wolf River Psychology | Home</title>
-      </Head>
-      <Layout page="home">
-      <SingleColumn sectionId="homeIntro">
-          <article className={classes.homeTextOuter}>
-            <div className={classes.homeTextInner}>
-              <p className={`${classes.homeText} margin-none h1 text-uppercase text-center`}>"Insight for your <em>present</em> to inspire your <em>future</em>."</p>
-            </div>
-          </article>
-        </SingleColumn>
+        <Head>
+            <title>Wolf River Psychology | Home</title>
+        </Head>
+        <Layout page="home">
+            <SingleColumn sectionId="homeIntro">
+                <article className={classes.homeTextOuter}>
+                    <div className={classes.homeTextInner}>
+                        <p
+                            className={`${classes.homeText} margin-none h1 text-uppercase text-center`}
+                        >
+                            "Insight for your <em>present</em> to inspire your{" "}
+                            <em>future</em>."
+                        </p>
+                    </div>
+                </article>
+            </SingleColumn>
 
-        <FullWidthColumn sectionId="homeServices">
-          <div className={classes.flexContainer}>
-            <div className={classes.innerContainer}>
-              <Link href="/services#anxiety-disorders" scroll={false}>
-                <a className={classes.iconLink}>
-                  <SpaSharpIcon className={classes.largeIcon} />
-                  <p className={`${classes.iconCaption} h1`}>Anxiety <br />Disorders</p>
-                </a>
-              </Link>
-            </div>
-            <div className={classes.innerContainer}>
-              <Link href="/services#ocd" scroll={false}>
-                <a className={classes.iconLink}>
-                  <PermIdentityIcon className={classes.largeIcon} />
-                  <p className={`${classes.iconCaption} h1`}>Obsessive <br />Compulsive Disorder</p>
-                </a>
-              </Link>
-            </div>
-            <div className={classes.innerContainer}>
-              <Link href="/services#sleep-disorders" scroll={false}>
-                <a className={classes.iconLink}>
-                  <NightsStayIcon className={classes.largeIcon} />
-                  <p className={`${classes.iconCaption} h1`}>Sleep <br />Disorders</p>
-                </a>
-              </Link>
-            </div>
-          </div>
-        </FullWidthColumn>
+            <FullWidthColumn sectionId="homeServices">
+                <div className={classes.flexContainer}>
+                    <div className={classes.innerContainer}>
+                        <Link href="/services#anxiety-disorders" scroll={false}>
+                            <a className={classes.iconLink}>
+                                <SpaSharpIcon className={classes.largeIcon} />
+                                <p className={`${classes.iconCaption} h1`}>
+                                    Anxiety <br />
+                                    Disorders
+                                </p>
+                            </a>
+                        </Link>
+                    </div>
+                    <div className={classes.innerContainer}>
+                        <Link href="/services#ocd" scroll={false}>
+                            <a className={classes.iconLink}>
+                                <PermIdentityIcon
+                                    className={classes.largeIcon}
+                                />
+                                <p className={`${classes.iconCaption} h1`}>
+                                    Obsessive <br />
+                                    Compulsive Disorder
+                                </p>
+                            </a>
+                        </Link>
+                    </div>
+                    <div className={classes.innerContainer}>
+                        <Link href="/services#sleep-disorders" scroll={false}>
+                            <a className={classes.iconLink}>
+                                <NightsStayIcon className={classes.largeIcon} />
+                                <p className={`${classes.iconCaption} h1`}>
+                                    Sleep <br />
+                                    Disorders
+                                </p>
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </FullWidthColumn>
 
-        <OffsetColumns sectionId="homeOffsetColumns">
-          <LeftOffset parent="homeLeft">
-            <p className="margin-top-0">Welcome to Wolf River Psychological Services.</p>
-            <p>Please take a moment to explore this site for additional information about me and the services offered.  I also encourage you to reach out via the contact page for any additional questions.</p>
-            <p>Thank you for your interest.</p>
-            <p className="margin-none">Sincerely,</p>
-            <p className="bold margin-none">Krystin Coldwell, Psy.D</p>
-            <p className="bold margin-none">Clinical Psychologist</p>
-          </LeftOffset>
-          <RightOffset parent="homeRight">
-            <Image className={classes.drColdwellImg} src={drColdwell} layout="responsive" title="Dr. Coldwell" />
-          </RightOffset>
-        </OffsetColumns>
+            <OffsetColumns sectionId="homeOffsetColumns">
+                <LeftOffset parent="homeLeft">
+                    <p className="margin-top-0">
+                        Welcome to Wolf River Psychological Services.
+                    </p>
+                    <p>
+                        Please take a moment to explore this site for additional
+                        information about me and the services offered. I also
+                        encourage you to reach out via the contact page for any
+                        additional questions.
+                    </p>
+                    <p>Thank you for your interest.</p>
+                    <p className="margin-none">Sincerely,</p>
+                    <p className="bold margin-none">Krystin Coldwell, Psy.D</p>
+                    <p className="bold margin-none">Clinical Psychologist</p>
+                </LeftOffset>
+                <RightOffset parent="homeRight">
+                    <Image
+                        className={classes.drColdwellImg}
+                        src={drColdwell}
+                        layout="responsive"
+                        title="Dr. Coldwell"
+                    />
+                </RightOffset>
+            </OffsetColumns>
 
-        <SingleColumn sectionId="homeQuote">
-          <Blockquote attribution="Irvin D. Yalom">The ultimate goal of therapy…it’s too hard a question. The words come to me like tranquility, like fulfillment, like realizing your potential.</Blockquote>
-        </SingleColumn>
-      </Layout>
+            <SingleColumn sectionId="homeQuote">
+                <Blockquote attribution="Irvin D. Yalom">
+                    The ultimate goal of therapy…it’s too hard a question. The
+                    words come to me like tranquility, like fulfillment, like
+                    realizing your potential.
+                </Blockquote>
+            </SingleColumn>
+        </Layout>
     </>
 );
 
