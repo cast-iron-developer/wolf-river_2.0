0d3cd7de59145e34043144c830d1f181c15aeacc
Sam ODonnell <sodonnell5214@gmail.com>
2021-08-24 09:38:04 -0400

    Adding exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,27 +1,29 @@
-const path = require('path')
-const withImages = require('next-images');
+const path = require("path");
+const withImages = require("next-images");
 module.exports = {
-  reactStrictMode: true,
-  webpack5: false,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    disableStaticImages: true
-  },
-  withImages,
-  target: "serverless",
-  exportPathMap: async function(
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': {page: '/'},
-      '/services': {page: '/services'},
-      '/about-psychotherapy': { page: '/about-psychotherapy'},
-      '/client': { page: '/client' },
-      '/professional-profile': { page: '/professional-profile'},
-      '/contact': { page: '/contact' },
-    }
-  }
-}
+    reactStrictMode: true,
+    webpack5: false,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "styles")],
+    },
+    images: {
+        disableStaticImages: true,
+        loader: "imgix",
+        path: "https://noop/",
+    },
+    withImages,
+    target: "serverless",
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            "/": { page: "/" },
+            "/services": { page: "/services" },
+            "/about-psychotherapy": { page: "/about-psychotherapy" },
+            "/client": { page: "/client" },
+            "/professional-profile": { page: "/professional-profile" },
+            "/contact": { page: "/contact" },
+        };
+    },
+};
